891087c340bb7fbfec06c55a30cef63d895d9c6b
Diogo Gomes <diogogomes.developer@gmail.com>
2024-07-30 20:41:16 +0200

    types

---
+++ src/types.js
@@ -0,0 +1,11 @@
+"use strict";
+// The primitives: string, number and boolean
+exports.__esModule = true;
+var firstName = "Diogo";
+var lastName = "Gomes";
+var fullName = "".concat(firstName, " ").concat(lastName);
+var age = 28;
+var isPerson = true;
+console.log("Name: ".concat(fullName));
+console.log("Age: ".concat(fullName));
+console.log(isPerson ? "Is person" : "Is not person");
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+// The primitives: string, number and boolean
+
+const firstName: string = "Diogo";
+const lastName: string = "Gomes";
+const fullName: string = `${firstName} ${lastName}`;
+
+const age: number = 28;
+
+const isPerson: boolean = true;
+
+console.log(`Name: ${fullName}`);
+console.log(`Age: ${fullName}`);
+console.log(isPerson ? "Is person" : "Is not person");
+
+export {};
