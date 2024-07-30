7d1d122c1d44f73c8645ad25e7f2057dd9a8a3d7
Diogo Gomes <diogogomes.developer@gmail.com>
2024-07-30 20:42:56 +0200

    functions

---
+++ src/functions.ts
@@ -0,0 +1,10 @@
+// Functions with typed parameters and return values
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+// Function called with arguments
+const sum = add(2, 3);
+
+console.log(sum);
